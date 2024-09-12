f37a3c0aa6c1ed60cd392d48abd5ad7292e8b1c4
liemak363 <liemak363work@gmail.com>
2024-09-13 00:14:41 +0700

    create 'products' view for client

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.render("./client/pages/home/index.pug");
 })
 
+app.get('/products', (req, res) => {
+    res.render("./client/pages/products/index.pug");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
