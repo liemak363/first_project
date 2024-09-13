2346de7b1fc2ea76d62606e61ad2cd9c139c3c59
liemak363 <liemak363work@gmail.com>
2024-09-13 16:21:21 +0700

    create 'home' route for client

--- routers/client/index_router.js
+++ routers/client/index_router.js
@@ -1,9 +1,8 @@
+const homeRouter = require("./home_router")
 const productsRouter = require("./products_router");
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.render("./client/pages/home/index.pug");
-    })
+    app.use('/', homeRouter);
     
     app.use('/products', productsRouter);
 }
