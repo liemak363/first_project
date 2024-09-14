279b25928f1da08bb3c1acbf37ea416a47145e63
liemak363 <liemak363work@gmail.com>
2024-09-14 11:19:51 +0700

    display products on 'products' page

--- controllers/client/products_controller.js
+++ controllers/client/products_controller.js
@@ -1,5 +1,19 @@
-module.exports.products = (req, res) => {
+const Product = require("../../models/product_model.js")
+
+module.exports.products = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    })
+    const newProducts = products.map((item) => {
+        item.priceNew = (item.price*(100 - item.discountPercentage)).toFixed(0);
+        return item;
+    })
+
+    console.log(newProducts);
+
     res.render("./client/pages/products/index.pug", {
-        pageTitle: "product"
+        pageTitle: "product",
+        products: newProducts
     });
 }
--- models/product_model.js
+++ models/product_model.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({ 
     title: String,
-    description: String, price: Number,
+    description: String, 
+    price: Number,
     discountPercentage: Number,
     stock: Number,
     thumbnail: String,
