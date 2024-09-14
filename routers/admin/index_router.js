161002ee89f90bb7081a23f7b61d9fb13df3492c
liemak363 <liemak363work@gmail.com>
2024-09-14 22:59:06 +0700

    add prefixAdmin to use in admin router in 'config' folder

--- routers/admin/index_router.js
+++ routers/admin/index_router.js
@@ -1,5 +1,7 @@
 const dashboardRouter = require("./dashboard_router.js")
+const systemConfig = require("../../config/system.js")
 
 module.exports = (app) => {
-    app.use('/admin/dashboard', dashboardRouter);
+    const PATH_ADMIN = systemConfig.prefixAdmin;
+    app.use(PATH_ADMIN + '/dashboard', dashboardRouter);
 }
