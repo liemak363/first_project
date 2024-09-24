897860b80bf124dc8d66c95d1b4a03a18a313a66
liemak363 <liemak363work@gmail.com>
2024-09-24 23:33:28 +0700

    install express-flash lib for notification

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 const app = express()
 
 const methodOverride = require('method-override')
@@ -20,6 +22,11 @@ const adminRoute = require("./routers/admin/index_router.js")
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
+
+const flash = require('express-flash')
+app.use(cookieParser('keyboard cat'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
 
 app.set('views', './views')
 app.set('view engine', 'pug')
