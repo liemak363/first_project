5b5aeb02774d103091c14e11c10c406900180b9b
liemak363 <liemak363work@gmail.com>
2024-10-01 17:59:03 +0700

    implement detail product for client

--- routers/client/products_router.js
+++ routers/client/products_router.js
@@ -5,4 +5,6 @@ const controller = require("../../controllers/client/products_controller.js")
 
 router.get('/', controller.products)
 
+router.get("/detail/:slug", controller.detail);
+
 module.exports = router
