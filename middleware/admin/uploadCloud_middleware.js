321e0a88d17e596d3ccbff02d7637ef934995581
liemak363 <liemak363work@gmail.com>
2024-10-20 10:54:11 +0700

    fix uploadCloud_middleware when no image

--- middleware/admin/uploadCloud_middleware.js
+++ middleware/admin/uploadCloud_middleware.js
@@ -10,27 +10,30 @@ cloudinary.config({
 // End Cloudanary config
 
 module.exports.upload = (req, res, next) => {
-    let streamUpload = (req) => {
-        return new Promise((resolve, reject) => {
-            let stream = cloudinary.uploader.upload_stream(
-                (error, result) => {
-                    if (result) {
-                        resolve(result);
-                    } else {
-                        reject(error);
+    if (req.file) {
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
                     }
-                }
-            );
-
-            streamifier.createReadStream(req.file.buffer).pipe(stream);
-        });
-    };
-
-    async function upload(req) {
-        let result = await streamUpload(req);
-        req.body[req.file.fieldname] = result.url;
-        next();
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            req.body[req.file.fieldname] = result.url;
+            next();
+        }
+    
+        upload(req);
     }
-
-    upload(req);
+    else next();
 }
