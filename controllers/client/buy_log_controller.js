f29600e13a7cc9cb406468629f3e04e3e366b083
liemak363 <liemak363work@gmail.com>
2025-01-09 09:19:39 +0700

    feat(buy_log): format createdAt

--- controllers/client/buy_log_controller.js
+++ controllers/client/buy_log_controller.js
@@ -3,6 +3,25 @@ const Product = require("../../models/product_model.js")
 
 const paginationHelper = require("../../helper/pagination.js")
 
+function convertToGmt7(dateString) {
+    // Parse the input date string as UTC
+    const utcDate = new Date(dateString);
+  
+    // Adjust to GMT+7 by adding 7 hours
+    const gmt7Date = new Date(utcDate.getTime() + 7 * 60 * 60 * 1000);
+  
+    // Extract day, month, year, hours, minutes, and seconds
+    const day = gmt7Date.getUTCDate().toString().padStart(2, '0');
+    const month = (gmt7Date.getUTCMonth() + 1).toString().padStart(2, '0'); // Months are 0-based
+    const year = gmt7Date.getUTCFullYear();
+    const hours = gmt7Date.getUTCHours().toString().padStart(2, '0');
+    const minutes = gmt7Date.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = gmt7Date.getUTCSeconds().toString().padStart(2, '0');
+  
+    // Format as dd/mm/yyyy hh:mm:ss
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
+
 module.exports.buyLog = async (req, res) => {
     // const userID = ....
 
@@ -24,6 +43,7 @@ module.exports.buyLog = async (req, res) => {
 
     // Update records synchronously
     for (const item of records) {
+        item.createdAtFormatted = convertToGmt7(item.createdAt);
         const product = await Product.findOne({ _id: item.productId });
         if (product) {
             item.title = product.title;
